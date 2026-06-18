023ec8edee9e52af2c1734353a24999a866fc707
Sudhanshu-2403 <sudhanshurana2403@gmail.com>
2026-06-18 12:53:06 -0400

    Update and rename Store Procedure Data Cleaning.sql to Stored Procedure Data Cleaning.sql

---
+++ Stored Procedure Data Cleaning.sql
@@ -0,0 +1,10 @@
+
+************************************************************
+******* Stored_Procedure to remove the outlier dates *******
+************************************************************
+  
+create procedure stg.data_cleaning_stg
+@end_date DATETIME2,
+@start_date DATETIME2
+as
+delete from stg.nyctaxi_yellow where tpep_pickup_datetime < @start_date and tpep_pickup_datetime > @end_date
